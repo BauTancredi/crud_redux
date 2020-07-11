6859923e597ccc31a2df1a24942e503cfdf63df0
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-11 16:26:59 -0300

    Added redux

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import Products from "./components/Products";
 import NewProduct from "./components/NewProduct";
 import EditProduct from "./components/EditProduct";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Products} />
-          <Route exact path="/products/new" component={NewProduct} />
-          <Route exact path="/products/edit/:id" component={EditProduct} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/products/new" component={NewProduct} />
+            <Route exact path="/products/edit/:id" component={EditProduct} />
+          </Switch>
+        </div>
+      </Provider>
     </BrowserRouter>
   );
 }
