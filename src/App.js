a07d1bdb29ee27a1276876ffc30ac2294741ac04
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-11 15:50:10 -0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Header from "./components/Header";
+import Products from "./components/Products";
+import NewProduct from "./components/NewProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
-    <Fragment>
+    <BrowserRouter>
       <Header />
-    </Fragment>
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route exact path="/products/new" component={NewProduct} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
