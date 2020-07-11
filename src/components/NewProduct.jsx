53ad5983af326f33a21dfecf17938f8be73e3e42
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-11 16:42:22 -0300

    CreateNewProductAction

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { createNewProductAction } from "../actions/productActions";
 
 const NewProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const dispatch = useDispatch();
+
+  const addProduct = (product) => dispatch(createNewProductAction(product));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name.trim() === "" || price <= 0) return;
+
+    addProduct({
+      name,
+      price,
+    });
+  };
   return (
     <div className="row justify-content-center">
       <div className="col-md-8">
@@ -10,7 +30,7 @@ const NewProduct = () => {
               Add new product
             </h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="form-group">
                 <label>Product Name</label>
                 <input
@@ -18,6 +38,8 @@ const NewProduct = () => {
                   className="form-control"
                   placeholder="Product Name"
                   name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
               <div className="form-group">
@@ -27,6 +49,8 @@ const NewProduct = () => {
                   className="form-control"
                   placeholder="Product Price"
                   name="price"
+                  value={price}
+                  onChange={(e) => setPrice(Number(e.target.value))}
                 />
               </div>
 
