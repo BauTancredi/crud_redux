e7e445fd1aee6cabc9500e5bfb09949e58df771e
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-12 15:53:30 -0300

    Displaying alerts

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -14,10 +14,19 @@ const Products = () => {
   }, []);
 
   const products = useSelector((state) => state.products.products);
+  const error = useSelector((state) => state.products.error);
+  const loading = useSelector((state) => state.products.loading);
+
   return (
     <Fragment>
       <h2 className="text-center my-5">Product Listing</h2>
+      {error ? (
+        <p className="font-weight-bold alert alert-danger text-center mt-4">
+          There has been an error
+        </p>
+      ) : null}
 
+      {loading ? <p className="text-center">Loading ...</p> : null}
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
           <tr>
@@ -27,7 +36,7 @@ const Products = () => {
           </tr>
         </thead>
         <tbody>
-          {products.lenght === 0
+          {products.length === 0
             ? "There are no products"
             : products.map((product) => (
                 <Product key={product.id} product={product} />
