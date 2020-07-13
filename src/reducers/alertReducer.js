4c44178a19fdc3f5e9329c5c02d75385857ccedf
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-13 13:12:25 -0300

    Adding a second reducer

---
+++ src/reducers/alertReducer.js
@@ -0,0 +1,12 @@
+import { SHOW_ALERT, HIDE_ALERTS } from "../types";
+
+const initialState = {
+  alert: null,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
